68a2795add73340899be75078f19fba30a80b098
Bradley Gaynor <BradleyGaynor@northwesternmutual.com>
2020-05-05 14:40:38 -0500

    Set up react app with tooling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SendMessage from './SendMessage'
+
+import { privateKey1, privateKey2, publicKey2 } from './test-keys'
+
+// const encryptedMessage = key2.encrypt('hello', 'base64')
+// console.log(encryptedMessage)
+// console.log(key2.decrypt(encryptedMessage, 'utf8'))
+
+
+// 1 side
+const messageToTwo = privateKey2.encrypt(privateKey1.encryptPrivate('hello', 'base64'), 'base64')
+
+// 2 side
+console.log(messageToTwo)
+
+// 
+const message = privateKey1.decryptPublic(privateKey2.decrypt(messageToTwo, 'utf8'), 'utf8')
+console.log(message)
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SendMessage />
     </div>
   );
 }
